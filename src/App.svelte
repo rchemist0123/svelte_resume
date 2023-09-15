46452f8db15f2713a028e39a14a2fd37a59ef679
rchemist0123 <aidenjang94@gmail.com>
2023-09-15 17:40:40 +0900

    update navbar active status

--- src/App.svelte
+++ src/App.svelte
@@ -7,30 +7,32 @@
   import ContactMe from './components/ContactMe.svelte';
   import { scrollIntoView } from './lib/JumpToSection.js';
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Skeleton, ImagePlaceholder, TextPlaceholder } from 'flowbite-svelte';
+  import '../app.css';
   let currentPage = 'home'; 
   let pages = {
     home: '#section-1',
-    resume: '#section-2',
+    about: '#section-2',
     portfolio: '#section-3',
     contact: '#section-4'
   }
-  $: activeUrl=pages[currentPage]
+  $:activeUrl=pages[currentPage]
+  let activeClass = 'text-primary-500 hover:text-primary-500 md:hover-text-primary-500';
+  let nonActiveClass = 'hover:text-primary-500 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-500 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
 </script>
-<div class="relative px-8">
+<div class="max-w-4xl">
   <Navbar let:hidden let:toggle navClass="sticky top-0">
     <NavBrand href="/">
-      <!-- <img src="/src/assets/svelte.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
       <span class="self-center whitespace-nowrap text-xl text-black font-semibold dark:text-white tracking-tight">Yeonhoon's Resumé</span>
     </NavBrand>
     <NavHamburger on:click={toggle} />
-    <NavUl {activeUrl} {hidden}>
-      <NavLi href="#section-1" on:click={ scrollIntoView }>Home</NavLi>
-      <NavLi href="#section-2" on:click={ scrollIntoView }>About</NavLi>
-      <NavLi href="#section-3" on:click={ scrollIntoView }>Portfolio</NavLi>
-      <NavLi href="#section-4" on:click={ scrollIntoView }>Contact</NavLi>
+    <NavUl {activeUrl} {hidden} {activeClass} {nonActiveClass}>
+      <NavLi href="#section-1" on:click={()=>currentPage = 'home'}>Home</NavLi>
+      <NavLi href="#section-2" on:click={()=>currentPage = 'about'}>About</NavLi>
+      <NavLi href="#section-3" on:click={()=>currentPage = 'portfolio'}>Portfolio</NavLi>
+      <NavLi href="#section-4" on:click={()=>currentPage = 'contact'}>Contact</NavLi>
     </NavUl>
   </Navbar>
-  <div class="pb-16 mt-48 max-w-3xl">
+  <div class="pb-16 mt-48 px-8">
     <Intro />
     <About />
     <PortfolioPage />
@@ -46,3 +48,4 @@
   {:else if currentPage === 'contact'}
   {/if}
 </main> -->
+
