22da3fcfd05961615f70745198b5a72c3e5cd244
rchemist0123 <aidenjang94@gmail.com>
2023-09-15 17:43:20 +0900

    update

--- src/lib/JumpToSection.js
+++ src/lib/JumpToSection.js
@@ -1,8 +1,10 @@
 // https://svelte.dev/repl/e073dc93fb3347848062a531271e497f?version=3.24.0
 export function scrollIntoView({ target }) {
-  const el = document.querySelector(target.getAttribute('href'));
+  let href = target.getAttribute('href');
+  const el = document.querySelector(href);
   if (!el) return;
   el.scrollIntoView({
+    block: 'start',
     behavior: 'smooth'
   });
 }
