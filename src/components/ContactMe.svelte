4168a48ab4633628e93e5630d1cec8554a723e66
rchemist0123 <aidenjang94@gmail.com>
2023-09-15 11:26:40 +0900

    location & mail

--- src/components/ContactMe.svelte
+++ src/components/ContactMe.svelte
@@ -2,29 +2,49 @@
   import { Input, Label, Helper, Button, P, Textarea } from 'flowbite-svelte'
 </script>
 
-<form action="" class="mt-60">
-  <div class="sec-out" id="section-4">
-    <div class="sec-in">
-      <Label for="email" class="mb-2">이메일 주소</Label>
-      <Input type="email" id="email" name="email" placeholder = "이메일 주소" required/>
-    </div>
-    <div class="sec-in">
-      <Label for="message" class="mb-2">메시지</Label>
-      <Textarea placeholder="메시지를 입력해주세요" rows=5 name="message" />
-      <Button type="submit">보내기</Button>
-    </div>
-
+<div class="mt-40 max-w-lg mx-auto"> 
+  <div class="">
+    <p class="font-bold text-left text-sm text-primary-600">CONTACT</p>
+    <p class="font-extrabold text-left text-md text-black my-3">Don't hesitate! Hit me up!👇🏻</p>
   </div>
-</form>
-<style>
-  .sec-out {
-    display: flex;
-    justify-content: center;
-    align-items: center; /* 수직 정렬 */
-    flex-direction: column; /* default: row */
-  }
-  .sec-in  {
-    margin-bottom: 1.5rem;
-    width: 80%
-  }
-</style>
+  <div class="grid grid-cols-2">
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl flex items-center space-x-5">
+      <div class="">
+        <svg class="w-6 h-6 text-primary-700 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+          <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z"/>
+        </svg>
+      </div>
+      <div class="text-left space-y-1">
+        <p class="text-sm font-bold text-black">Location</p>
+        <p class="text-xs text-slate-500">Seongnam, South Korea</p>
+      </div>
+    </div>
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl flex items-center space-x-5">
+      <div class="">
+        <svg class="w-6 h-6 text-primary-700 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="m9.978 13.233 9.392-6.668a1.945 1.945 0 0 0-.186-.177L11.2.65A2 2 0 0 0 8.815.638L.8 6.4a1.928 1.928 0 0 0-.207.2l9.385 6.633Z"/>
+          <path d="M11.181 14.864a2.007 2.007 0 0 1-2.382-.014L0 8.627V18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8.573l-8.819 6.29Z"/>
+        </svg>
+      </div>
+      <div class="text-left space-y-1">
+        <p class="text-sm font-bold text-black">Mail</p>
+        <p class="text-xs text-slate-500">danjang0123@gmail.com</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- <form action="" class="mt-20">
+    <div  id="section-4">
+      <div class="sec-in">
+        <Label for="email" class="mb-2">이메일 주소</Label>
+        <Input type="email" id="email" name="email" placeholder = "이메일 주소" required/>
+      </div>
+      <div class="">
+        <Label for="message" class="mb-2">메시지</Label>
+        <Textarea placeholder="메시지를 입력해주세요" rows=5 name="message" />
+        <Button type="submit">보내기</Button>
+      </div>
+    </div>
+  </form> -->
+</div>
+
