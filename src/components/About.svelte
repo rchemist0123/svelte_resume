22b21a9f0e8ca15bb5a32cd0d840d84ab344a215
rchemist0123 <aidenjang94@gmail.com>
2023-09-15 17:41:45 +0900

    resume download butotn

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -6,17 +6,17 @@
     <p class="font-extrabold text-left text-sm text-primary-600">ABOUT ME</p>
     <p class="font-semibold text-left text-sm my-3">A passionate data analyst based in Seongnam, South Korea📍</p>
     <p class="text-slate-400 text-left text-xs my-3">Mainly use R. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima minus expedita cum quas aut iusto harum unde dolorum totam vitae odio, debitis natus id, aspernatur non! Cum hic voluptatem delectus?</p>
-    <div class="">
-      <button class="group px-auto space-x-1 rounded-xl border-primary-400 bg-white 
-        hover:bg-primary-600 hover:border-none hover:ring-2 hover:ring-primary-600
-        focus:ring-primary-500 focus:bg-primary-400 group-hover:text-white
-        active:scale-95 active:bg-primary-800"
+    <div class="text-left">
+      <button class="group w-24 h-10 px-2 py-1 space-x-1 rounded-xl border-primary-400 bg-white 
+        hover:scale-105 hover:outline-none hover:border-primary-400
+        focus:ring-primary-500 focus:bg-primary-400 group-hover:text-white 
+        active:scale-95"
       >
       <a class="text-black" href="/src/assets/test.txt" download="test.txt">
-        <svg class="w-3 h-3 text-primary-600 group-hover:text-white dark:text-white inline-block" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 18">
+        <svg class="w-3 h-3 text-primary-600  dark:text-white inline-block" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 18">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 1v11m0 0 4-4m-4 4L4 8m11 4v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3"/>
         </svg>
-        <span class="text-xs font-bold group-hover:text-white">Resumé</span>
+        <span class="text-xs font-bold text-black">Resumé</span>
         </a>
       </button>
     </div>
