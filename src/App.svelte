a1784acb11460dd605fdb9e03a87c10b37a6d477
rchemist0123 <aidenjang94@gmail.com>
2023-09-18 16:28:04 +0900

    Resume dispatch

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onMount, tick, beforeUpdate, afterUpdate } from 'svelte';
   import Footer from './components/Footer.svelte';
   import Intro from './components/Intro.svelte';
   import About from './components/About.svelte';
@@ -8,6 +9,7 @@
   import { scrollIntoView } from './lib/JumpToSection.js';
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Skeleton, ImagePlaceholder, TextPlaceholder } from 'flowbite-svelte';
   import '../app.css';
+  // import { divs } from './main.js'
   let currentPage = 'home'; 
   let pages = {
     home: '#section-1',
@@ -24,27 +26,45 @@
   $:activeUrl=pages[currentPage]
   let activeClass = 'text-primary-500 hover:text-primary-500 md:hover-text-primary-500';
   let nonActiveClass = 'hover:text-primary-500 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-500 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
+  
+  onMount(()=>{
+    async function check(){
+      await tick();
+      const sections = document.querySelectorAll('section[id]')
+      console.log(sections[0].children)
+    }
+    check()
+  })
+  let showResume = false;
+  function openResume(event){
+    showResume = true;
+  }
+  function closeResume(event){
+    showResume = false;
+  }
+
 </script>
 <div class="max-w-4xl">
-  <Navbar let:hidden let:toggle navClass="sticky top-0">
+  <Navbar let:hidden let:toggle navClass="sticky top-0 navigation">
     <NavBrand href="/">
       <span class="self-center whitespace-nowrap text-xl text-black font-semibold dark:text-white tracking-tight">Yeonhoon's Resumé</span>
     </NavBrand>
     <NavHamburger on:click={toggle} />
     <NavUl {activeUrl} {hidden} {activeClass} {nonActiveClass}>
       {#each pages_arr as page}
-        <NavLi href={page.href} on:click={()=>currentPage=page.name}>{page.text}</NavLi>
+        <NavLi navClass="" href={page.href} on:click={()=>currentPage=page.name}>{page.text}</NavLi>
       {/each}
     </NavUl>
   </Navbar>
   <div class="pb-16 mt-48 px-8">
-      <Intro />
-      <About />
-      <PortfolioPage />
-      <ContactMe />
+      {#if showResume === true}
+        <Resume on:backToPage={closeResume}/>
+      {:else}
+        <Intro />
+        <About on:resumetest={openResume}/>
+        <PortfolioPage />
+        <ContactMe />
+      {/if}
   </div>
   <Footer />
 </div>
-<style>
-
-</style>
