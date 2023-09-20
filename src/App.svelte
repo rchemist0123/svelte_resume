44bf587d74e42a98b433ae7eade206879c5c4c42
rchemist0123 <aidenjang94@gmail.com>
2023-09-20 17:54:26 +0900

    split app into app and nav

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script>
   import { onMount, tick, beforeUpdate, afterUpdate } from 'svelte';
-  import Footer from './components/Footer.svelte';
+  import NavPage from './components/NavPage.svelte';
   import Intro from './components/Intro.svelte';
   import About from './components/About.svelte';
   import Resume from './components/Resume.svelte';
   import PortfolioPage from "./components/PortfolioPage.svelte";
   import ContactMe from './components/ContactMe.svelte';
+  import Footer from './components/Footer.svelte';
   import { scrollIntoView } from './lib/JumpToSection.js';
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, DarkMode, Button } from 'flowbite-svelte';
   import '../app.css';
@@ -28,6 +29,21 @@
   let nonActiveClass = 'hover:text-primary-500 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-500 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
   
   onMount(()=>{
+    const hamburger = document.querySelector("#hamburger");
+    const menu = document.querySelector("#menu")
+    hamburger.addEventListener("click",()=>{
+      menu.classList.toggle("hidden")
+      hamburger.classList.toggle('bg-white')
+    })
+    
+    const hrefs = document.querySelectorAll("li > a")
+    hrefs.forEach(href => {
+      href.addEventListener('click', (e) => {
+        hrefs.forEach((el)=> el.classList.remove("text-primary-500"));
+        href.classList.add("text-primary-500");
+      })
+    });
+
     async function check(){
       await tick();
       // const sections = document.querySelectorAll('section[id]')
@@ -45,39 +61,46 @@
     const body = document.querySelector('body')
     body.classList.toggle('dark')
   }
+  
+  $:checkHref = () => {
 
+  }
+  
+  
+  
+  
 </script>
-<div>
-  <Navbar let:hidden let:toggle navClass="sticky top-0 navigation dark:bg-slate-900">
-    <NavBrand href="/">
-      <span class="self-center whitespace-nowrap text-xl text-black font-semibold dark:text-white tracking-tight">Yeonhoon's Resumé</span>
-    </NavBrand>
-    <div class="flex md:order-2">
-      <Button color="none" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
+
+<!-- <Navbar let:hidden let:toggle navClass="sticky top-0 navigation dark:bg-slate-900">
+  <NavBrand href="/">
+    <span class="self-center whitespace-nowrap text-xl text-black font-semibold dark:text-white tracking-tight">Yeonhoon's Resumé</span>
+  </NavBrand>
+  <div class="flex md:order-2">
+    <Button color="none" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
+      <DarkMode />
+    </Button>
+    <div class="hidden relative md:block">
+      <div class="flex absolute inset-y-0 left-0 items-center pl-3">
         <DarkMode />
-      </Button>
-      <div class="hidden relative md:block">
-        <div class="flex absolute inset-y-0 left-0 items-center pl-3">
-          <DarkMode />
-        </div>
       </div>
-      <NavHamburger on:click={toggle} />
     </div>
-    <NavUl {activeUrl} {hidden} {activeClass} {nonActiveClass}>
-      {#each pages_arr as page}
-      <NavLi navClass="" href={page.href} on:click={()=>currentPage=page.name}>{page.text}</NavLi>
-      {/each}
-    </NavUl>
-  </Navbar>
-  <div class="pb-16 mt-48 px-8">
-      {#if showResume === true}
-        <Resume on:backToPage={closeResume}/>
-      {:else}
-        <Intro />
-        <About on:resumetest={openResume}/>
-        <PortfolioPage />
-        <ContactMe />
-      {/if}
+    <NavHamburger on:click={toggle} />
   </div>
+  <NavUl {activeUrl} {hidden} {activeClass} {nonActiveClass}>
+    {#each pages_arr as page}
+    <NavLi navClass="" href={page.href} on:click={()=>currentPage=page.name}>{page.text}</NavLi>
+    {/each}
+  </NavUl>
+</Navbar> -->
+<div class="sm:h-screen relative">
+  {#if showResume === true}
+    <Resume on:backToPage={closeResume}/>
+  {:else}
+    <NavPage />
+    <Intro />
+    <About on:resumetest={openResume}/>
+    <PortfolioPage />
+    <ContactMe />
+  {/if}
   <Footer />
 </div>
---
+++ src/components/NavPage.svelte
@@ -0,0 +1,49 @@
+<script>
+    let pages_arr = [
+    {name:'home',href:'#section-1', text: 'Home'},
+    {name:'about',href:'#section-2', text: 'About'},
+    {name:'portfolio',href:'#section-3', text: 'Portfolio'},
+    {name:'contact',href:'#section-4', text: 'Contact'},
+  ]
+
+  function toggleDark(){
+    const body = document.querySelector('body')
+    body.classList.toggle('dark')
+  }
+  let currentPage = 'home'; 
+</script>
+<nav class="w-full fixed top-0 bg-white z-10 dark:bg-slate-900">
+    <div class="container mx-auto py-5 flex items-center justify-between">
+      <div class="flex items-center gap-2">
+        <img class="w-8" src="/src/assets/favicon.png" alt="" />
+        <span class="text-3xl font-caveat font-extrabold italic text-primary-500 dark:text-white">Yeonhoon's Resumé</span>
+      </div>
+      <ul class="hidden md:flex space-x-10 text-gray-600 dark:text-white font-bold text-sm uppercase">
+        {#each pages_arr as page}
+          <li class="group hover:text-primary-500 active:text-primary-500">
+            <a class="group-hover:text-primary-500 group-active:text-primary-500" href={page.href} id={page.name} on:click={()=>{currentPage=page.name}}>{page.text}</a>
+          </li>
+        {/each}
+      </ul>
+      <button on:click={toggleDark}>
+          <img class="hidden md:block w-5 cursor-pointer border-none active:outline-none focus:outline-none" src="/src/assets/moon.png" alt="" />
+      </button>
+      <!-- Hamberger -->
+      <div id="hamburger" class="space-y-1 md:hidden cursor-pointer z-20">
+        <div class="w-6 h-1 bg-black"></div>
+        <div class="w-6 h-1 bg-black"></div>
+        <div class="w-6 h-1 bg-black"></div>
+      </div>
+      <ul
+        id="menu"
+        class="hidden bg-white absolute left-0 top-0 w-full p-10 rounded-b-3xl space-y-10 text-black text-center
+        dark:bg-slate-500 dark:text-white"
+      >
+        {#each pages_arr as page}
+          <li class="group hover:text-primary-500 active:text-primary-500">
+            <a class="group-hover:text-primary-500 group-active:text-primary-500" href={page.href} on:click={()=>{currentPage=page.name}}>{page.text}</a>
+          </li>
+        {/each}
+      </ul>
+    </div>
+  </nav>
