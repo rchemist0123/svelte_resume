d000b9e986f24d8277eb239024018a4e92c951b8
rchemist0123 <aidenjang94@gmail.com>
2023-10-16 08:50:10 +0900

    update

---
+++ src/lib/store.js
@@ -0,0 +1,9 @@
+import { writable } from "svelte/store";
+
+export const secLoc = writable(0);
+// const temp = document.querySelector("#section-2").offsetTop;
+// export const section2start = readable(temp)
+
+// section2start.subscribe(console.log)
+
+
