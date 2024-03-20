aff33f132f1eb1f94420028a842bcc6b0fdb0827
rchemist0123 <aidenjang0123@gmail.com>
2024-03-20 13:06:25 +0900

    poster presentation list

---
+++ src/components/Presentation.svelte
@@ -0,0 +1,20 @@
+<script>
+  let lists = [
+    {dates:'2023-10-25', subject: 'Sepsis associated AKI'},
+    {dates:'2022-10-21', subject: 'New-onset Atrial fibrillation'},
+  ]
+</script>
+<div>
+  <div>
+    <ul>
+      {#each lists as list}
+      <li>
+        subject:{list.subject}
+      </li>
+      <li>
+        dates: {list.dates}
+      </li>
+      {/each}
+    </ul>
+  </div>
+</div>
